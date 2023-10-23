a941c638b5f04e2472a4fd3371923416cf42a381
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-23 22:39:52 +0500

    question added

---
+++ index.js
@@ -0,0 +1,38 @@
+const questionss = [
+    {
+        question: "Which is the largest animal in the world?",
+        answer: [
+            {text: "Shark", correct: false },
+            {text: "Blue Whale", correct: true },
+            {text: "Elephant", correct: false },
+            {text: "Giraffe", correct: false },
+        ]
+    }
+    {
+        question: "Which is the smallest country in the world?",
+        answer: [
+            {text: "Vatican City", correct: false },
+            {text: "Bhutan", correct: true },
+            {text: "Nepal", correct: false },
+            {text: "Siri Lanka", correct: false },
+        ]
+    }
+    {
+        question: "Which is the largest desert in the world?",
+        answer: [
+            {text: "Kalahari", correct: false },
+            {text: "Gobi", correct: true },
+            {text: "Sahara", correct: false },
+            {text: "Antarctica", correct: false },
+        ]
+    }
+    {
+        question: "Which is the smallest continent in the world?",
+        answer: [
+            {text: "Asia", correct: false },
+            {text: "Australia", correct: true },
+            {text: "Arctic", correct: false },
+            {text: "Africa", correct: false },
+        ]
+    }
+];
