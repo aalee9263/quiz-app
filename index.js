bf27964935cc18bb368f6aa1bb6338c47e677fe0
aalee9263 <144584171+aalee9263@users.noreply.github.com>
2023-10-23 23:24:28 +0500

    js

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
-const questionss = [
-    {
-        question: "Which is the largest animal in the world?",
-        answer: [
-            {text: "Shark", correct: false },
-            {text: "Blue Whale", correct: true },
-            {text: "Elephant", correct: false },
-            {text: "Giraffe", correct: false },
-        ]
-    }
-    {
-        question: "Which is the smallest country in the world?",
-        answer: [
-            {text: "Vatican City", correct: false },
-            {text: "Bhutan", correct: true },
-            {text: "Nepal", correct: false },
-            {text: "Siri Lanka", correct: false },
-        ]
-    }
-    {
-        question: "Which is the largest desert in the world?",
-        answer: [
-            {text: "Kalahari", correct: false },
-            {text: "Gobi", correct: true },
-            {text: "Sahara", correct: false },
-            {text: "Antarctica", correct: false },
-        ]
-    }
-    {
-        question: "Which is the smallest continent in the world?",
-        answer: [
-            {text: "Asia", correct: false },
-            {text: "Australia", correct: true },
-            {text: "Arctic", correct: false },
-            {text: "Africa", correct: false },
-        ]
-    }
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answers: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the smallest country in the world?",
+    answers: [
+      { text: "Vatican City", correct: false },
+      { text: "Bhutan", correct: true },
+      { text: "Nepal", correct: false },
+      { text: "Siri Lanka", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest desert in the world?",
+    answers: [
+      { text: "Kalahari", correct: false },
+      { text: "Gobi", correct: true },
+      { text: "Sahara", correct: false },
+      { text: "Antarctica", correct: false },
+    ],
+  },
+  {
+    question: "Which is the smallest continent in the world?",
+    answers: [
+      { text: "Asia", correct: false },
+      { text: "Australia", correct: true },
+      { text: "Arctic", correct: false },
+      { text: "Africa", correct: false },
+    ],
+  },
 ];
+
+const questionElement = document.querySelector("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  let currentQuestion = question[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButton.appendChild(button);
+  });
+}
